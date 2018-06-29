48d3367872d0fc18f20553fe9d0e0acd54bf5976
Emmanuel <ogoma.emmanuel@gmail.com>
2018-06-29 08:06:56 +0300

    added filtrer

--- app.js
+++ app.js
@@ -1,58 +1,81 @@
 let app = angular.module("appModule", ['ngRoute']);
 
-// app.config(['$routeProvider',function($routeProvider) {
-//     $routeProvider.when('/products', {
-//        templateUrl: 'index.html',
-//     }).
-//     $routeProvider.when('/trending', {
-//         templateUrl: 'trending.html',
-//      }).
-//     when('/manafucturers', {
-//        templateUrl: 'manafucturers.html',
-//     }).otherwise({
-//        redirectTo: '/manafucturers'
-//     });
-     
-//  }]);
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+
+        when('/products', {
+            templateUrl: 'products.html',
+            controller: 'ProductsController'
+        }).
+
+        when('/about', {
+            templateUrl: 'about.html',
+            controller: "AboutController"
+        }).
+        otherwise({
+            redirectTo: '/products'
+        });
+
+}]);
 
 
 app.controller("ProductsController", function ($scope) {
-    $scope.product={}
-    $scope.productDetail = {};
-    $scope.products = [{
-        name: "IPod Nano - 8GB",
-        price: 199.00,
-        quantity: 30,
-        description: "LEGO Star Wars: The Complete Saga - Xbox 360"
+    $scope.product = {};
+    $scope.search = null;
 
-    },
+    $scope.productDetail = {};
+    $scope.products = [
+        //     {
+        //     name: "IPod Nano - 8GB",
+        //     price: 199.00,
+        //     quantity: 30,
+        //     description: "LEGO Star Wars: The Complete Saga - Xbox 360"
+
+        // },
     ]
-    $scope.addProduct = function(){
+    $scope.addProduct = function () {
         $scope.products.push($scope.product);
         console.log($scope.product);
-        $scope.product={};
+        $scope.product = {};
     }
     $scope.viewProductDetail = function (product) {
-        $scope.productDetail =product;
-
-
-       
-
+        $scope.productDetail = product;
         console.log(productDetail);
-
-        // $('#productDetailModal').foundation('open');
     }
 });
 
-// app.controller("ManafucturersController",function($scope){
-//     $scope.manufactures=[{
+app.filter("productLegth", function () {
+    return function (productList) {
+        if (productList.length > 0) {
+            return "You have added" + productList.length;
+        }
+        else {
+            return "No product Added"
+        }
+    }
+})
+app.filter("productFilter", function () {
+    return function (productList, search_term) {
+        console.log(search_term);
+        if (search_term == null || search_term== "undefined") {
+            return productList;
+        }
+        else {
+            productList = productList.filter(product => product.name.toLowerCase().indexOf(search_term.toLowerCase()) !== -1)
+           
+            return productList;
+        }
+    }
+})
+app.controller("AboutController", function ($scope) {
+    $scope.companyDetails = {
+        name: "Cytonn Investemnts"
 
-//     }]
-
-// })
+    }
+})
 // app.controller("TrendingController",function($scope){
 //     $scope.manufactures=[{
-        
+
 //     }]
 
 // })
